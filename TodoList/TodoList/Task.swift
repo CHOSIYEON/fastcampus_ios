8c0cf6dfc8fd9574c2a64c9404a5286dce54b824
조시연 <chosiyeonn@gmail.com>
2022-02-15 02:53:39 +0900

    Add TodoList Function(1) - Add Todo

---
+++ TodoList/TodoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by Cho Si Yeon on 2022/02/15.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var done: Bool
+}
--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
     }
 
-
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        
+        // handler : registerButton 을 눌렀을 때, 실행될 함수 : 클로저
+        // 클래스처럼 클로저는 참조 타입이기 때문에 클로저의 본문에서 self 로 인스턴스를 캡처할 때 강한 순환 참조가 발생할 수 있음
+        // 강한 순환 참조란 ARC 의 단점이기도 한데, 두개의 객체가 상호 참조하는 경우 강한 순환 참조가 만들어짐
+        // 이렇게 강한 순환 참조가 만들어지게 되면 이에 연관된 객체들은 레퍼런스의 카운트가 0에 도달하기 않게 되고 메모리 누수가 발생하게 됨
+        // 이런 점을 해결하기 위해서 클로저의 선언부에서 캡처 목록을 정의하는 것으로 해결할 수 있음
+        // weak, unknown 으로 캡처 목록을 정의함
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        })
+        
+        // cancleButton 눌렀을 때, 아무것도 실행되지 않음 -> nil
+        let cancleButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(cancleButton)
+        alert.addAction(registerButton)
+        
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "할 일을 입력해주세요."
+        })
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    // 각 섹션에 표시할 행의 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+    }
+    // 특정 row 에 그리기 위한 cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // dequeueReusableCell : 지정된 재사용 가능한 셀의 객체를 반환하고 이를 테이블뷰에 추가하는 역할
+        // dequeueReusableCell 메소드를 사용하면 큐를 이용해 셀을 재사용 하게 됨
+        // 테이블 뷰에 10개의 셀이 있다면 10개의 셀을 만들어 메모리에 올림
+        // 그럼 테이블 뷰에 1000개의 셀이 있다면 1000개의 셀을 만들고 메모리에 할당해야하나? 만약 이렇다면 불필요한 메모리 낭비가 심해질 것
+        // 큐를 사용해 셀을 재사용하면 이러한 메모리 낭비를 방지할 수 있음
+        // 현재 내가 볼 수 있는 화면에 다섯개의 셀이 있다면 앱은 다섯개의 데이터만 받아와 메모리에 로드하게 되고,
+        // 스크롤을 내리면서 점점 많은 데이터를 다섯개의 셀을 재사용하여 보여주게 됨
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
+    
+    
+}
