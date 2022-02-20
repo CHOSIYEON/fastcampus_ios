d3ea4320d056b2d272d0ef031fa9286b50280558
조시연 <chosiyeonn@gmail.com>
2022-02-21 00:07:43 +0900

    Add Diary Function(1) - Add Diary, UserDefault

---
+++ Basic/Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Cho Si Yeon on 2022/02/20.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
--- Basic/Diary/Diary/WriteDiaryViewController.swift
+++ Basic/Diary/Diary/WriteDiaryViewController.swift
@@ -7,11 +7,95 @@
 
 import UIKit
 
+protocol WriteDiaryViewDelegate: AnyObject {
+    func didSelectRegister(diary: Diary)
+}
+
 class WriteDiaryViewController: UIViewController {
 
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentsTextView: UITextView!
+    @IBOutlet var dateTextField: UITextField!
+    @IBOutlet var confirmButton: UIBarButtonItem!
+    
+    private let datePicker = UIDatePicker()
+    private var diaryDate: Date? // 선택한 날짜 저장 상수
+    weak var delegate: WriteDiaryViewDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureContentsTextView()
+        self.configureDatePicker()
+        self.configureInputField()
+        self.confirmButton.isEnabled = false
 
-        // Do any additional setup after loading the view.
+    }
+    
+    private func configureInputField() {
+        self.contentsTextView.delegate = self
+        self.titleTextField.addTarget(self, action: #selector(titleTextFieldDidChange(_:)), for: .editingChanged)
+        self.dateTextField.addTarget(self, action: #selector(dateTextFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    private func configureContentsTextView() {
+        let boarderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        self.contentsTextView.layer.borderColor = boarderColor.cgColor
+        self.contentsTextView.layer.borderWidth = 0.5
+        self.contentsTextView.layer.cornerRadius = 5.0
+        
+    }
+    
+    private func configureDatePicker() {
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        // addTarget() 은 UIController 객체가 이벤트에 응답하는 방식을 선택해주는 메소드
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChange(_:)), for: .valueChanged)
+        self.datePicker.locale = Locale(identifier: "ko_KR")
+        self.dateTextField.inputView = self.datePicker
+    }
+    
+    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
+        guard let title = self.titleTextField.text else { return }
+        guard let contents = self.contentsTextView.text else { return }
+        guard let date = self.diaryDate else { return }
+        
+        let diary = Diary(title: title, contents: contents, date: date, isStar: false)
+        self.delegate?.didSelectRegister(diary: diary)
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formmater = DateFormatter() // Date 타입을 사람이 읽을 수 있는 문자열 형태로 반환 혹은 그 반대
+        formmater.dateFormat = "yyyy년 MM월 dd일(EEEEE)" // EEEEE -> 요일을 한글자만 표시
+        formmater.locale = Locale(identifier: "ko_KR")
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formmater.string(from: datePicker.date)
+        self.dateTextField.sendActions(for: .editingChanged)
+    }
+    
+    @objc private func titleTextFieldDidChange(_ textField: UITextField) {
+        self.validateInputField()
+    }
+    
+    @objc private func dateTextFieldDidChange(_ textField: UITextField) {
+        self.validateInputField()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // 빈 화면을 터치했을 때, 키보드가 사라짐
+        self.view.endEditing(true)
+    }
+    
+    private func validateInputField() {
+        // a ?? b -> nil 병합 연산자 : a 가 nil 이면 b 반환
+        self.confirmButton.isEnabled = !(self.titleTextField.text?.isEmpty ?? true) && !(self.dateTextField.text?.isEmpty ?? true) && !self.contentsTextView.text.isEmpty
+    }
+}
+
+extension WriteDiaryViewController: UITextViewDelegate {
+    // textview 에 text 가 입력될때마다 호출됨
+    func textViewDidChange(_ textView: UITextView) {
+        self.validateInputField()
     }
 }
