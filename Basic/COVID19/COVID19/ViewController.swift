059702fb42591185e6d1f791a5e09cca346251ab
chosiyeonn <chosiyeonn@gmail.com>
2022-02-27 17:02:07 +0900

    Create COVID19

---
+++ Basic/COVID19/COVID19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by Cho Si Yeon on 2022/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
